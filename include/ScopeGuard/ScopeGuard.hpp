37284e611bf86d1ce7e0569d3dfb09667f3b2e81
權少 <ljqican@163.com>
2019-12-17 20:44:29 +0800

    第一次提交

---
+++ include/ScopeGuard/ScopeGuard.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#ifndef SCOPE_GUARD_HPP
+#define SCOPE_GUARD_HPP
+#include <functional>
+class ScopeGuard
+{
+public:
+    explicit ScopeGuard(std::function<void()> _onExit)
+        :onExit(_onExit), dismissed(false)
+    {
+
+    }
+
+    ~ScopeGuard()
+    {
+        if (!dismissed)
+        {
+            onExit();
+        }
+    }
+
+    void dissmiss()
+    {
+        dismissed = true;
+    }
+
+    ScopeGuard(const ScopeGuard&) = delete;
+    ScopeGuard& operator=(const ScopeGuard&) = delete;
+private:
+    std::function<void()> onExit;
+    bool dismissed;
+};
+
+#define SCOPE_GUARD_LINENAME_CAT(name, line) name##line
+#define SCOPE_GUARD_LINENAME(name, line) SCOPE_GUARD_LINENAME_CAT(name, line)
+#define ON_SCOPE_EXIT(callback) ScopeGuard SCOPE_GUARD_LINENAME(name, __LINE__)(callback)
+#endif // !SCOPE_GUARD_HPP
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "../include/ScopeGuard/ScopeGuard.hpp"
+
+int main()
+{
+    ON_SCOPE_EXIT([]()
+    {
+        std::cout << "exit" << std::endl;
+    });
+    return 0;
+}

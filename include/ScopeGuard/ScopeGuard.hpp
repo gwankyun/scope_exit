288fd4025f08993a112c7766d712fa27b61f0ea9
權少 <ljqican@163.com>
2020-06-29 23:34:12 +0800

    增加對C++98的支持

--- include/ScopeGuard/ScopeGuard.hpp
+++ include/ScopeGuard/ScopeGuard.hpp
@@ -2,20 +2,44 @@
 #ifndef SCOPE_GUARD_HPP
 #define SCOPE_GUARD_HPP
 #include <functional>
+
 class ScopeGuard
 {
 public:
-    explicit ScopeGuard(std::function<void()> _onExit)
-        :onExit(_onExit), dismissed(false)
-    {
 
+#ifdef __cpp_lambdas
+    explicit ScopeGuard(std::function<void()> onExit_)
+        : onExit(onExit_), dismissed(false), base(NULL)
+    {
+    }
+#endif // __cpp_lambdas
+
+    template<typename CallBack, typename T>
+    explicit ScopeGuard(CallBack callback_, T& value_)
+        : dismissed(false), base(new Derived<T, CallBack, void(*)()>(callback_, value_))
+    {
+    }
+
+    template<typename Procedure>
+    explicit ScopeGuard(Procedure procedure_)
+        : dismissed(false), base(new Derived<int, void(*)(int), Procedure>(procedure_))
+    {
     }
 
     ~ScopeGuard()
     {
         if (!dismissed)
         {
-            onExit();
+            if (base != NULL)
+            {
+                delete base;
+            }
+            else
+            {
+#ifdef __cpp_lambdas
+                onExit();
+#endif // __cpp_lambdas
+            }
         }
     }
 
@@ -24,14 +48,67 @@ public:
         dismissed = true;
     }
 
-    ScopeGuard(const ScopeGuard&) = delete;
-    ScopeGuard& operator=(const ScopeGuard&) = delete;
 private:
+
+    ScopeGuard(const ScopeGuard&);
+    ScopeGuard& operator=(const ScopeGuard&);
+
+    class Base
+    {
+    public:
+        Base()
+        {
+        }
+
+        virtual ~Base()
+        {
+        }
+
+    private:
+
+    };
+
+    template<typename T, typename CallBack,typename Procedure>
+    class Derived : public Base
+    {
+    public:
+        Derived(CallBack callback_, T& value_) : value(&value_), callback(callback_), hasCallBack(true), hasProcedure(false)//, procedure(NULL)
+        {
+        }
+
+        Derived(Procedure procedure_) : value(NULL), hasCallBack(false), hasProcedure(true), procedure(procedure_)//, procedure(NULL)
+        {
+        }
+
+        ~Derived()
+        {
+            if (hasCallBack)
+            {
+                callback(*value);
+            }
+
+            if (hasProcedure)
+            {
+                procedure();
+            }
+        }
+
+    private:
+        T* value;
+        CallBack callback;
+        bool hasCallBack;
+        Procedure procedure;
+        bool hasProcedure;
+    };
+
+#ifdef __cpp_lambdas
     std::function<void()> onExit;
+#endif // __cpp_lambdas
     bool dismissed;
+    Base* base;
 };
 
 #define SCOPE_GUARD_LINENAME_CAT(name, line) name##line
 #define SCOPE_GUARD_LINENAME(name, line) SCOPE_GUARD_LINENAME_CAT(name, line)
-#define ON_SCOPE_EXIT(callback) ScopeGuard SCOPE_GUARD_LINENAME(name, __LINE__)(callback)
+#define ON_SCOPE_EXIT(...) ScopeGuard SCOPE_GUARD_LINENAME(name, __LINE__)(##__VA_ARGS__)
 #endif // !SCOPE_GUARD_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,57 @@
 ﻿#include <iostream>
 #include "../include/ScopeGuard/ScopeGuard.hpp"
 
+class Object
+{
+public:
+    Object()
+    {
+    }
+
+    ~Object()
+    {
+        std::cout << __LINE__ << std::endl;
+    }
+
+private:
+
+};
+
+void deleteObject(Object*& obj)
+{
+    delete obj;
+}
+
+void bye()
+{
+    std::cout << __func__ << std::endl;
+}
+
 int main()
 {
     ON_SCOPE_EXIT([]()
     {
         std::cout << "exit" << std::endl;
     });
+
+    auto obj = new Object();
+
+    //{
+    //    ON_SCOPE_EXIT(obj);
+    //}
+
+    {
+        ON_SCOPE_EXIT(deleteObject, obj);
+    }
+
+    {
+        ON_SCOPE_EXIT(bye);
+    }
+
+    //if (obj == NULL)
+    //{
+    //    std::cout << "NULL" << std::endl;
+    //}
+
     return 0;
 }

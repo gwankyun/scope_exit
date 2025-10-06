44fe48d801f1264c4198929d5602f90da251643f
權少 <ljqican@163.com>
2025-10-06 19:44:31 +0800

    添加clang-mingw支持。

--- scope_exit/scope_exit.h
+++ scope_exit/scope_exit.h
@@ -17,5 +17,5 @@
 #endif
 
 #ifndef ON_SCOPE_EXIT
-#  define ON_SCOPE_EXIT(...) lite::scope_exit UNIQUE_NAME(scope_exit, UNIQUE_ID)(lite::make_callback(##__VA_ARGS__))
+#  define ON_SCOPE_EXIT(...) lite::scope_exit UNIQUE_NAME(scope_exit, UNIQUE_ID)(lite::make_callback(__VA_ARGS__))
 #endif

10c3c5cb1791bc246beb8d53334be3c57ba09554
權少 <ljqican@163.com>
2025-12-30 22:14:46 +0800

    測試重構為一個main.cpp文件。

--- src/main_decl.hpp
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-#include <catch2/catch_session.hpp>
-//#include <catch2/../catch2/catch_session.hpp>
-#include <catch2/catch_test_macros.hpp>
-
-#include <scope_exit/local_function.hpp> // LOCAL_FUNCTION_*
-#include <scope_exit/scope_exit.h>       // ON_SCOPE_EXIT
--- src/main_header.cpp
+++
@@ -1,3 +0,0 @@
-﻿#include "main_decl.hpp"
-
-#include "main_impl.hpp"
